070932a0ab27ae10735a0404f455330a6fc9c7ea
Danex2 <khadane.miller@gmail.com>
2021-03-03 00:48:24 -0500

    adding more dependencies + setting base configs for API server

--- plantain/go.mod
+++ plantain/go.mod
@@ -1,3 +1,5 @@
 module github.com/nulfrost/vend
 
 go 1.15
+
+require github.com/gofiber/fiber/v2 v2.5.0
--- plantain/main.go
+++ plantain/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"time"
 
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/nulfrost/vend/routes"
+)
 
 func main() {
-	fmt.Println("This is the plantain API server!")
+
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,PUT,POST,DELETE",
+	}))
+	app.Use(logger.New(logger.Config{
+		Next:         nil,
+		Format:       "[${time}] ${status} - ${latency} ${method} ${path}\n",
+		TimeFormat:   "15:04:05",
+		TimeZone:     "Local",
+		TimeInterval: 500 * time.Millisecond,
+		Output:       os.Stderr,
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Vend API server running")
+	})
+
+	app.Get("/create", routes.CreateSingleAd)
+	app.Get("/update", routes.UpdateSingleAd)
+	app.Get("/delete", routes.DeleteSingleAd)
+
+	app.Listen(":3070")
 }
