bc7cfa78a5a785bdec89e2d769411cdd5b693c5b
Danex2 <khadane.miller@gmail.com>
2021-03-07 16:59:44 -0500

    created footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-between px-10 py-16 md:flex-row md:items-center bg-primary-500">
+      <Link href="/">
+        <a className="mb-4 font-bold text-white text-heading-1 md:mb-0">Vend</a>
+      </Link>
+      <ul className="flex space-x-3 text-white">
+        <li>
+          <Link href="/listings">
+            <a>Listings</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <a
+            href="https://github.com/nulfrost/vend"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Github
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
 
 const IndexPage = () => (
   <div>
     <Navbar />
+    <Footer />
   </div>
 );
 
